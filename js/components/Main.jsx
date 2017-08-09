b33cfbba3e4fab3da2bc5903cf5acb0b5e156eff
Tomasz Wilczek <twilczek88@gmail.com>
2017-08-09 22:15:55 +0200

    implemented rover change

--- js/components/Main.jsx
+++ js/components/Main.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Menu from './Main/Menu.jsx';
 import Gallery from './Main/Gallery.jsx';
+import api from '../utilities/api.js';
 
-export default class Main extends React.Component {
+export default class Main extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            links: [],
+            rover: 'curiosity'
+        }
+    }
+
+    changeRover = rover => {
+        this.setState({rover: rover});
+        api.getLinks(rover)
+        .then( r => {
+            this.setState({ links: r })
+        });
+    }
+
+    componentWillMount(){
+        api.getLinks(this.state.rover)
+        .then( r => {
+            this.setState({ links: r })
+        });
     }
 
     render() {
         return <main>
-            <Menu/>
-            <Gallery links = {this.props.links}/>
+            <Menu changeRover = {this.changeRover}/>
+            <Gallery links = {this.state.links}/>
         </main>
     }
 }
--- js/components/Main/Menu.jsx
+++ js/components/Main/Menu.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default class Menu extends React.Component {
+export default class Menu extends Component {
+
+    handleRoverChange = rover => {
+        if(typeof this.props.changeRover == 'function') {
+            this.props.changeRover(rover);
+        } else {
+            console.error('no function parsed in <Menu/>');
+        }
+    }
 
     render() {
-        return <p>Jestem Menu</p>
+        return <ul>
+            Change rover:
+            <li onClick={ e => this.handleRoverChange('curiosity') }>Curiosity</li>
+            <li onClick={ e => this.handleRoverChange('opportunity') }>Opportunity</li>
+            <li onClick={ e => this.handleRoverChange('spirit') }>Spirit</li>
+        </ul>
     }
 }
